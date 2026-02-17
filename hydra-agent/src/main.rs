994dada871db94b70886aabbad112e92236e3d6c
Xoce <josemaria.micoli@gmail.com>
2026-02-17 12:22:16 -0300

    Update LICENSE and source/docs headers

--- hydra-agent/src/main.rs
+++ hydra-agent/src/main.rs
@@ -1,3 +1,18 @@
+
+/*
+Copyright (c) 2026 José María Micoli
+Licensed under AGPLv3
+
+You may:
+✔ Study
+✔ Modify
+✔ Use for internal security testing
+
+You may NOT:
+✘ Offer as a commercial service
+✘ Sell derived competing products
+*/
+
 // Project: Hydra-Worm Agent
 // Integrity Status: MODULAR_CONSOLIDATION_MOD
 
